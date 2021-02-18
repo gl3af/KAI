21dbbfe5904a9d6725a304c060652bcc0bb9554d
gl3af <nik_baranov_2002@mail.ru>
2021-02-18 12:23:40 +0300

    Fixed version

--- FirstCourse/SecondSemester/Labs/Lab1/1.cpp
+++ FirstCourse/SecondSemester/Labs/Lab1/1.cpp
@@ -5,60 +5,60 @@ int main()
 {
     double x1, x2;
     int roots = 0;
-    int a,b,c;
+    double a,b,c;
     std::cout << "Введите a: ";
     std::cin >> a;
     std::cout << "Введите b: ";
     std::cin >> b;
     std::cout << "Введите c: ";
     std::cin >> c;
-    // x + 2 >= 0
-    double D = sqrt(pow(a - c, 2) + 4 * (a * c - b));
-    if (D == 0)
+    // x + a >= 0
+    double root_of_D = sqrt(pow(a - c, 2) + 4 * (a * c - b));
+    if (root_of_D == 0)
     {
         x1 = (c - a) / 2.0;
-        if (x1 >= -1 * a)
+        if (x1 >= -1 * a && x1 != c)
         {
             ++roots;
             std::cout << "Корень уравнения: " << x1 << "\n";
         }
     }
-    else if(D > 0)
+    else if(root_of_D > 0)
     {
-        x1 = (c - a + D) / 2.0;
-        x2 = (c - a - D) / 2.0;
-        if (x1 >= -1 * a)
+        x1 = (c - a + root_of_D) / 2.0;
+        x2 = (c - a - root_of_D) / 2.0;
+        if (x1 >= -1 * a && x1 != c)
         {
             ++roots;
             std::cout << "Корень уравнения: " << x1 << "\n";
         }
-        if (x2 >= -1 * a)
+        if (x2 >= -1 * a && x1 != c)
         {
             ++roots;
             std::cout << "Корень уравнения: " << x2 << "\n";
         }
     }
-    // x + 2 < 0
-    D = sqrt(pow(a - c, 2) + 4 * (a * c + b));
-    if (D == 0)
+    // x + a < 0
+    root_of_D = sqrt(pow(a - c, 2) + 4 * (a * c + b));
+    if (root_of_D == 0)
     {
         x1 = (c - a) / 2.0;
-        if (x1 < -1 * a)
+        if (x1 < -1 * a && x1 != c)
         {
             ++roots;
             std::cout << "Корень уравнения: " << x1 << "\n";
         }
     }
-    else if(D > 0)
+    else if(root_of_D > 0)
     {
-        x1 = (c - a + D) / 2.0;
-        x2 = (c - a - D) / 2.0;
-        if (x1 < -1 * a)
+        x1 = (c - a + root_of_D) / 2.0;
+        x2 = (c - a - root_of_D) / 2.0;
+        if (x1 < -1 * a && x1 != c)
         {
             ++roots;
             std::cout << "Корень уравнения: " << x1 << "\n";
         }
-        if (x2 < -1 * a)
+        if (x2 < -1 * a && x1 != c)
         {
             ++roots;
             std::cout << "Корень уравнения: " << x2 << "\n";
